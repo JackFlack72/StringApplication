f5f36125c6493cdd361f42e02dcf3edbec638dc6
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-09-25 12:38:38 -0500

    third commit

--- src/main/java/com/pluralsight/Reciept.java
+++ src/main/java/com/pluralsight/Reciept.java
@@ -2,6 +2,9 @@ package com.pluralsight;
 
 public class Reciept {
     public static void main(String[] args) {
-
+        String itemName = "bananas";
+        double itemPrice = 1.99;
+        int quantity = 5;
+        System.out.println("You bought "+quantity+" "+itemName+" for "+itemPrice);
     }
 }
