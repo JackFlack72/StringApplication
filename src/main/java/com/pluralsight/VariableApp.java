98499a12b056b210e6be4ff5bc6ab1e705d843ec
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-09-25 11:01:51 -0500

    first commit

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+        String favColor = "blue";
+        String yearStarted = "2025";
+        char middleInitial = 'G';
+        Integer howManyPets = 1;
+        String messageOrSaying = "Have a nice day!";
+        System.out.println("My favorite color is "+favColor+".");
+        System.out.println("The year I started this class was in "+yearStarted+".");
+        System.out.println("My middle initial is "+middleInitial+".");
+        System.out.println("I have a total of "+howManyPets+" but I work at an animal shelter so they're all technically my pets.");
+        System.out.println("One thing I always say is:"+messageOrSaying);
+    }
+}
