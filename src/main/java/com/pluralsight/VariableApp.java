dc358ab1233fc334f6a821c9b36f8761fe823a43
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-09-25 12:29:51 -0500

    first commit

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -5,12 +5,20 @@ public class VariableApp {
         String favColor = "blue";
         String yearStarted = "2025";
         char middleInitial = 'G';
-        Integer howManyPets = 1;
+        int howManyPets = 1;
         String messageOrSaying = "Have a nice day!";
         System.out.println("My favorite color is "+favColor+".");
         System.out.println("The year I started this class was in "+yearStarted+".");
         System.out.println("My middle initial is "+middleInitial+".");
         System.out.println("I have a total of "+howManyPets+" but I work at an animal shelter so they're all technically my pets.");
         System.out.println("One thing I always say is:"+messageOrSaying);
+        int numberOfDaysPerWeek = 7;
+        double priceOfCoffee = 4.99;
+        char myFavoriteLetter = 'L';
+        boolean isItRaining = false;
+        System.out.println("There are "+numberOfDaysPerWeek+" days in the week.");
+        System.out.println("The price is "+priceOfCoffee+" for one cup of coffee");
+        System.out.println("My favorite letter is "+myFavoriteLetter+" because it's the first letter of my first name!");
+        System.out.println("True or False: Is it raining? "+isItRaining);
     }
 }
